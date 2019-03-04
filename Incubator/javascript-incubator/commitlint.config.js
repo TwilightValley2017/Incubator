672df8bd7e026de9c09779e894ce446edd13ee32
yangyang <tenderyoung@yeah.net>
2019-03-04 15:52:20 +0800

    refactor: modify the commit lint config

--- Incubator/javascript-incubator/commitlint.config.js
+++ Incubator/javascript-incubator/commitlint.config.js
@@ -7,7 +7,6 @@ module.exports = {
         'type-enum': [2, 'always', [
             "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
         ]],
-        'scope-empty': [2, 'never'],
         'subject-full-stop': [0, 'never'],
         'subject-case': [0, 'never']
     }};
