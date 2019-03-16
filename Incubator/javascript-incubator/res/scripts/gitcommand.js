78aae08798ef4b6fed14e6847db5b0521d0eda15
twilightvalley <tenderyoung@yeah.net>
2019-03-16 21:22:47 +0800

    refactor: clear removed repo and nginx config

--- Incubator/javascript-incubator/res/scripts/gitcommand.js
+++
@@ -1,7 +0,0 @@
-console.log("Edit on master branch");
-console.log("Edit on master branch again");
-console.log("Edit on master branch by shadow ninja");
-console.log("A");
-console.log("B");
-console.log("B1");
-console.log("C");
